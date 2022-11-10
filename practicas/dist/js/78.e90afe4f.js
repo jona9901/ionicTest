607798c7d637d5c81dc33de2cb33885ff49f284e
unknown <CIAC1@CIAC1-0MA.academic.mixcoac.upmx.mx>
2022-11-09 20:26:47 -0600

    deploys

---
+++ practicas/dist/js/78.e90afe4f.js
@@ -0,0 +1,6 @@
+"use strict";(self["webpackChunkpracticas"]=self["webpackChunkpracticas"]||[]).push([[78],{6078:(e,t,s)=>{s.r(t),s.d(t,{startStatusTap:()=>o});var n=s(65),r=s(8338),a=s(6587);
+/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */
+const o=()=>{const e=window;e.addEventListener("statusTap",(()=>{(0,n.wj)((()=>{const t=e.innerWidth,s=e.innerHeight,o=document.elementFromPoint(t/2,s/2);if(!o)return;const c=(0,r.a)(o);c&&new Promise((e=>(0,a.c)(c,e))).then((()=>{(0,n.Iu)((async()=>{c.style.setProperty("--overflow","hidden"),await(0,r.s)(c,300),c.style.removeProperty("--overflow")}))}))}))}))}}}]);
+//# sourceMappingURL=78.e90afe4f.js.map
